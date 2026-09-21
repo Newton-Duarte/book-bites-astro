164d9abd0f9b002eaa58f59197906ab6ee036f9b
Newton Duarte <newton_@hotmail.com>
2026-09-21 11:31:18 -0300

    feat: add astro react integration and a likes component to render in the book detail page

---
+++ src/components/Likes.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function Likes() {
+  const [likes, setLikes] = useState(3);
+
+  const handleClick = () => {
+    setLikes((prev) => prev + 1);
+  };
+  return (
+    <div className="likes">
+      <p>{likes} people like this review.</p>
+      <button onClick={handleClick}>Like</button>
+    </div>
+  );
+}
